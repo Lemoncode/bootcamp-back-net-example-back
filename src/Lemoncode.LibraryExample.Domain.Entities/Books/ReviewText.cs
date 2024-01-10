1a9082ee637477e6c489fd86862be601161db853
Juanjo M <juanjo@jmontiel.es>
2024-01-10 05:31:36 +0000

    Hag oque ReviewText herede de ValueObject.

--- src/Lemoncode.LibraryExample.Domain.Entities/Books/ReviewText.cs
+++ src/Lemoncode.LibraryExample.Domain.Entities/Books/ReviewText.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 
 namespace Lemoncode.LibraryExample.Domain.Entities.Books;
 
-public record class ReviewText
+public record class ReviewText : ValueObject
 {
 
 	public string Text { get; private set; }
 
 	public ReviewText(string text)
 	{
-		Text = text ?? throw new ArgumentNullException(nameof(text));
+		if (text is null || text.Length < 10 || text.Length > 4000)
+		{
+			AddValidationError("The review text must contain between 10 and 4000 charracters.");
+		}
+
+		Validate();
+
+		this.Text = text;
 	}
 }
